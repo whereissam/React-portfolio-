564f0f4dd62084f24dff8cabce1f7d5443669d67
Sam <63341022+whereissam@users.noreply.github.com>
2021-08-08 22:28:24 +0800

    Component & scroll effect
    scss, scroll-snap-type,  scroll-snap-align
    Create change page effect

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './navbar.scss'
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            
+        </div>
+    )
+}
---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './portfolio.scss'
+export default function Portfolio() {
+    return (
+        <div className='portfolio'>
+            
+        </div>
+    )
+}
