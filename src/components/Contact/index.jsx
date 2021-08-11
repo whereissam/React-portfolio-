aabf0e869da0b51f48c6c79768b4baac08f8936f
Sam <63341022+whereissam@users.noreply.github.com>
2021-08-11 16:39:47 +0800

    Update contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './contact.scss'
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setMessage(true)
+    }
     return (
         <div className='contact' id='contact'>
-            
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Email' name="" id="" />
+                    <textarea name="" placeholder='Message' id="" cols="30" rows="10"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I'll reply ASAP :)</span>}
+                </form>
+            </div>
         </div>
     )
 }
